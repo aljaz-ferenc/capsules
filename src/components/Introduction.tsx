ed83a26624ef546b1a2306088ca9e347784325d4
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2025-05-01 15:32:40 +0200

    welcome, introduction style

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,28 +1,35 @@
 import {motion, useScroll, useTransform} from 'motion/react'
+import {useRef} from "react";
 
 export default function Introduction() {
-    const {scrollYProgress} = useScroll()
-    const clipPath = useTransform(scrollYProgress, [0, 1.3], ['inset(0% 0% 0% 0%)', 'inset(100% 0% 0% 0%)'])
+    const containerRef = useRef(null)
+    const {scrollYProgress} = useScroll({target: containerRef, offset: ['5% 100%', '100% 50%']})
+    const clipPath = useTransform(scrollYProgress, [0, 1], ['inset(0% 0% 0% 0%)', 'inset(100% 0% 0% 0%)'])
 
     return (
-        <section>
-            <div className='relative mb-50'>
+        <section className='mt-70 px-10'>
+            <div className='relative mb-40'>
                 <motion.div
                     className='bg-[#181717] absolute inset-0 opacity-90'
                     style={{clipPath}}
                 />
-                <p className='text-[85.5px] leading-22'>
-                    Welcome to a world of wild California
-                    desert with Capsules®, where you willdesert with Capsules®, where you will
-                    discover exquisite nature observing itdiscover exquisite nature observing it
-                    from capsule houses, nestled in thefrom capsule houses, nestled in the
-                    one of the most breathtakingone of the most breathtaking
-                    destination on the United States.
+                <p className='text-[88.5px] leading-22 tracking-[1px] max-w-[35ch]' ref={containerRef}>
+                    Welcome to a world of wild California desert with Capsules®, where you will
+                    discover exquisite nature observing
+                    from capsule houses, nestled in the
+                    one of the most breathtaking
+                    destination on the United States.destination on the United States.
                 </p>
             </div>
-            <div className='flex pb-50'>
-                <img src="welcome-1.webp" alt="" className='rounded-full'/>
-                <img src="welcome-2.webp" alt="" className='rounded-full'/>
+            <div className='flex items-start'>
+                <div className='flex pb-50 w-full'>
+                    <img src="welcome-1.webp" alt="" className='rounded-full'/>
+                    <img src="welcome-2.webp" alt="" className='rounded-full'/>
+                </div>
+                <div className='w-full'>
+                <p className='text-[#b1a696] text-[50px] leading-13 max-w-'>A place where you can be with yourself and your loved ones.
+                    A place where you can experience unforgettable desert things.</p>
+                </div>
             </div>
         </section>
     )
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,5 +1,19 @@
-export default function Welcome(){
-    return <section className='w-screen h-screen p-2.5'>
-        <img src="cap1.jpg" alt="hero image" className='rounded-[60px] w-full h-full object-cover'/>
-    </section>
+export default function Welcome() {
+    return (
+        <section className="w-screen h-screen relative text-[#F4EFE7] p-2.5">
+            <img
+                src="cap1.jpg"
+                alt="hero image"
+                className="z-10 rounded-[60px] w-full h-full object-cover"
+            />
+
+            <div className="p-8 absolute top-0 h-full w-full z-20 flex flex-col justify-between">
+                <h1 className="block text-[250px] leading-[1] tracking-[-8px]">Capsules</h1>
+                <div className='flex justify-between w-full text-xl items-end pb-4'>
+                    <span className='text-[50px] max-w-[13ch] leading-12'>Closer to Nature—Closer to Yourself</span>
+                    <span className='max-w-[36ch] text-lg leading-6 font-bold mr-2 pb-4'>Spend unforgettable and remarkable time in the Californian desert with—Capsules.</span>
+                </div>
+            </div>
+        </section>
+    );
 }
