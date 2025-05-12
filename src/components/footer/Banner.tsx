77040a38d3032fd620f881b920d8ba9c3dc37493
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2025-05-12 10:41:21 +0200

    animate banner image scale on scroll progress

--- src/components/footer/Banner.tsx
+++ src/components/footer/Banner.tsx
@@ -1,11 +1,26 @@
+import { useScroll, useTransform, motion, useSpring } from "motion/react";
+import { useRef } from "react";
+
 export default function Banner() {
+	const imageRef = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: imageRef,
+		offset: ["start end", "start start"],
+	});
+	const scale = useTransform(scrollYProgress, [0, 1], [1.3, 1]);
+	const springedScale = useSpring(scale, { damping: 20 });
+
 	return (
 		<div className="px-[10px] w-full h-[413px] relative ">
-			<img
-				src="terrace.webp"
-				alt=""
-				className="h-full object-cover rounded-[30px]"
-			/>
+			<div className="rounded-[30px] w-full h-full overflow-hidden">
+				<motion.img
+					ref={imageRef}
+					src="terrace.webp"
+					alt=""
+					className=" object-cover h-full"
+					style={{ scale: springedScale }}
+				/>
+			</div>
 			<h1 className="section-title absolute top-1/2 left-1/2 -translate-1/2">
 				Capsules
 			</h1>
